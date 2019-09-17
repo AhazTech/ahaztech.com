426b8a0a3ca0040b77b14603b7a27162df01eca6
codeninja <codeninja0x01@gmail.com>
2019-09-17 17:19:30 +0300

    Add build to git

---
+++ build/precache-manifest.f5eb382c58dba768778341421be03266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8fa2f81e2dfa7db1fb43f3f3cdba1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044c92dbe7e9156efaed",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f227828b59d1ceda44b0",
+    "url": "/static/js/2.4a91568b.chunk.js"
+  },
+  {
+    "revision": "044c92dbe7e9156efaed",
+    "url": "/static/js/main.bf4d2bcd.chunk.js"
+  },
+  {
+    "revision": "08cc54f8c84afa93cfac",
+    "url": "/static/js/runtime~main.97d44daf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
